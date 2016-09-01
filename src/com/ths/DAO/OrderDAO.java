b82b1b0875d18f4174ac066ee649c5a9ff1bd36b
Sathya Narayanan <sathyanarayanan.anbu@valeo.com>
2016-09-01 13:50:58 +0530

    test

--- src/com/ths/DAO/OrderDAO.java
+++ src/com/ths/DAO/OrderDAO.java
@@ -13,18 +13,18 @@ public class OrderDAO extends AbstractDao {
 
     private static final Logger log = Logger.getLogger(OrderDAO.class.getName());
 
-    public Example findById(long id) {
+    public OrderJDO findById(long id) {
         log.log(FINER, "Loading example with ID {0}", id);
-        return ofy.load().key(Key.create(Example.class, id)).now();
+        return ofy.load().key(Key.create(OrderJDO.class, id)).now();
     }
 
-    public Example findByName(String name) {
-        return ofy.load().type(Example.class).filter("name =", name).first().now();
+    public OrderJDO findByName(String name) {
+        return ofy.load().type(OrderJDO.class).filter("name =", name).first().now();
     }
 
-    public List<Example> findAllUsers() {
+    public List<OrderJDO> findAllUsers() {
         log.log(FINER, "Loading all examples.");
-        return ofy.load().type(Example.class).list();
+        return ofy.load().type(OrderJDO.class).list();
     }
 
     public OrderJDO save(OrderJDO order) {
@@ -34,7 +34,7 @@ public class OrderDAO extends AbstractDao {
     }
     
     public void deleteAll() {
-        List<Key<Example>> keys = ofy.load().type(Example.class).keys().list();
+        List<Key<OrderJDO>> keys = ofy.load().type(OrderJDO.class).keys().list();
         ofy.delete().entities(keys).now();
     }
 }
