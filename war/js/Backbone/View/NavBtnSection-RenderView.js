f9b408b9efb1b9944cba16c664783a2cf60fb776
Sathya Narayanan <sathyanarayanan.anbu@valeo.com>
2016-09-01 20:06:28 +0530

    test

--- war/js/Backbone/View/NavBtnSection-RenderView.js
+++ war/js/Backbone/View/NavBtnSection-RenderView.js
@@ -20,49 +20,83 @@ BackboneData.Views.NavBtnSectionview = Backbone.View.extend({
     },
     validateOrder: function() {
     	var setFlag = false;
-        if (validate.getInstance().formordiv('orderdetailview')) {
-            console.log("trfdsf");
-            this.saveOrder();
-        } 
-        else 
-        {
-            $('#orderdetailview').find('.selectpicker').each(function() {
-                if ($(this).hasClass('error')) 
-                {
-                	if(!$(this).parent().parent().hasClass('hide'))
-                	{
-                		setFlag = true;
-                		$(this).selectpicker('setStyle', 'error', 'add');
-                	}	
-                }
-            });
-            $('#orderdetailview').find('.form-control').each(function() {
-                if ($(this).hasClass('error')) 
-                {
-                	if(!$(this).parent().hasClass('hide'))
-                	{
-                		setFlag = true;
-                	}
-                	else
-                	{
-                		$(this).removeClass('error');
-                	}	
-                }
-            });
-        }
-        if(!setFlag)
-        {
-        	this.saveOrder();
-        }	
-        if(validate.getInstance().formordiv('component-section')) {
-        	 console.log("validating component section");
-         } else {
-             $('#component-section').find('.selectpicker').each(function() {
-                 if ($(this).hasClass('error')) {
-                 	$(this).selectpicker('setStyle','error','add');
-                 }
-             })
-        }
+    	if(document.URL.indexOf('#createorder')!=-1)
+    	{
+    		if (validate.getInstance().formordiv('orderdetailview')) {
+                console.log("trfdsf");
+                this.saveOrder();
+            } 
+            else 
+            {
+                $('#orderdetailview').find('.selectpicker').each(function() {
+                    if ($(this).hasClass('error')) 
+                    {
+                    	if(!$(this).parent().parent().hasClass('hide'))
+                    	{
+                    		setFlag = true;
+                    		$(this).selectpicker('setStyle', 'error', 'add');
+                    	}	
+                    }
+                });
+                $('#orderdetailview').find('.form-control').each(function() {
+                    if ($(this).hasClass('error')) 
+                    {
+                    	if(!$(this).parent().hasClass('hide'))
+                    	{
+                    		setFlag = true;
+                    	}
+                    	else
+                    	{
+                    		$(this).removeClass('error');
+                    	}	
+                    }
+                });
+            }
+    		if(!setFlag)
+            {
+            	this.saveOrder();
+            }
+    	}	
+    	else if(document.URL.indexOf('#componentdetails')!=-1)
+    	{
+    		if(validate.getInstance().formordiv('component-section')) {
+    			$('#statusmsg').html("Component created successfully");
+    			$('#componentidfield').val("C0000001");
+                $('#statusLoader').removeClass('hide');
+                $('#statusLoader .voicebox-content').addClass('in');
+                setTimeout(function(){ $('#statusLoader .voicebox-content').removeClass('in'); }, 3000);
+            } 
+           else 
+           {
+                $('#component-section').find('.selectpicker').each(function() {
+               	 if ($(this).hasClass('error')) 
+                    {
+                    	if(!$(this).parent().parent().hasClass('hide'))
+                    	{
+                    		setFlag = true;
+                    		$(this).selectpicker('setStyle', 'error', 'add');
+                    	}	
+                    }
+                });
+                $('#component-section').find('.form-control').each(function() {
+                    if ($(this).hasClass('error')) 
+                    {
+                    	if(!$(this).parent().hasClass('hide'))
+                    	{
+                    		setFlag = true;
+                    	}
+                    	else
+                    	{
+                    		$(this).removeClass('error');
+                    	}	
+                    }
+                });
+           }
+    		if(!setFlag)
+            {
+            	this.saveComponent();
+            }
+    	}	
     },
     saveOrder: function() {
     	 $('#statusmsg').html("Saving your order..");
@@ -104,7 +138,43 @@ BackboneData.Views.NavBtnSectionview = Backbone.View.extend({
         var fotrade = $('#fotrade').val();
         var adv = $('#adv').val();
         var projmanager = $('#projmanager').val();
-
+         console.log("Site_Workshop_Prototype :: "+Site_Workshop_Prototype);
+         console.log("geoSite :: "+Geosite);
+         console.log("no_prototype_order :: "+No_Prototype_Order)
+         console.log("proto_Type :: "+Proto_Type);
+         console.log("pcc :: "+pcccheckbox);
+         console.log("open_Order :: "+openordercheckbox);
+         console.log("frittage ::"+frittagecheckbox);
+         console.log("e52 :: "+e52checkbox);
+         console.log("intraLE :: "+intralecheckbox);
+         console.log("type_of_the_Prototype_Order :: "+Type_of_the_Prototype_Order);
+         console.log("date_of_the_Order :: ");
+         console.log("no_customerOrder :: "+no_customer_order);
+         console.log("customerOrderAttachment :: "+customer_order_list)
+         console.log("customer_Name :: "+Customer_Name);
+         console.log("customer_Code  ::"+Customer_Code);
+         console.log("branch_Code :: "+Branch_Code);
+         console.log("provider_Code ::"+Provider_Code);
+          console.log("final_Delivery_Address ::"+Final_Delivery_Address);
+           console.log("additional_Address ::"+Additional_Address);
+            console.log("site_Address :: "+Site_Address);
+             console.log("plateform :: "+Plateform);
+             console.log("incoterms :: "+Incoterms);
+             console.log("place :: "+Place);
+             console.log("customer_Receiver_Name :: "+Customer_Receiver_Name);
+             console.log("customer_Receiver_Telephone ::"+Customer_Receiver_Telephone);
+             console.log("allocation_of_turnover ::"+Allocation_of_turnover);
+             console.log("requester ::"+requester);
+             console.log("cadTeamMember_MEPStudy :: "+mepstudy);
+             console.log("qualityTeamMember :: "+quality);
+             console.log("protoWorkshop ::"+protoworkshop);
+             console.log("managementController :: "+controlmanagement);
+             console.log("kamCommerce :: "+fotrade);
+             console.log("salesAdministrator_ADV :: "+adv);
+             console.log("projectManager ::"+projmanager);
+             console.log("order_Status ::"+Order_Status);
+             console.log("total_Order_Amount "+Total_Order_Amount);
+             
         var orderDetailsObj = new BackboneData.Models.OrderDetailModel({
             site_Workshop_Prototype: Site_Workshop_Prototype,
             geoSite: Geosite,
@@ -133,13 +203,13 @@ BackboneData.Views.NavBtnSectionview = Backbone.View.extend({
             customer_Receiver_Telephone: Customer_Receiver_Telephone,
             allocation_of_turnover: Allocation_of_turnover,
             requester:requester,
-            mep_Study: mepstudy,
-            quality: quality,
-            proto_Workshop: protoworkshop,
-            control_Management: controlmanagement,
-            fo_Trade: fotrade,
-            adv: adv,
-            project_Manager: projmanager,      	
+            cadTeamMember_MEPStudy: mepstudy,
+            qualityTeamMember: quality,
+            protoWorkshop: protoworkshop,
+            managementController: controlmanagement,
+            kamCommerce: fotrade,
+            salesAdministrator_ADV: adv,
+            projectManager: projmanager,      	
             order_Status: "Draft",
             total_Order_Amount: 0,
         });
@@ -158,6 +228,8 @@ BackboneData.Views.NavBtnSectionview = Backbone.View.extend({
                 console.log(model);
                 console.log(xhr);
                 console.log(options);
+                window.orderStatus = "saved";
+                setTimeout(function(){ $('#statusLoader .voicebox-content').removeClass('in'); }, 3000);
             }
         });
     }
