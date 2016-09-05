b63d97ddb6241f2bcb51146262b6d1e1a5931c2a
Sathya Narayanan <sathyanarayanan.anbu@valeo.com>
2016-09-05 16:08:03 +0530

    test

--- war/js/Loader/initializer.js
+++ war/js/Loader/initializer.js
@@ -1,23 +1,53 @@
-
-$(document).ready(function() 
+$(document).ready(function()
 {
-	$(document).on("keyup", ".form-control", function(e) {
-	    $(this).removeClass('error');
-	});
-	$(document).on("click", ".language li a", function(e) {
-	    var languageChanged = $.trim($(this).text());
-	    if (window.language != languageChanged) {
-	        bootbox.confirm("Any unsaved changes will be lost. Are you sure you want to change the language?", function(result) {
-	            if (result) {
-	                console.log("ste ::" + languageChanged);
-	                loadAllViewsAgainBasedOnLanguage(languageChanged);
-	            }
-	        });
-	    }
-	});
+    $(document).on("keyup", ".form-control", function(e)
+    {
+        $(this).removeClass('error');
+    });
+    $(document).on("click", ".language li a", function(e)
+    {
+        var languageChanged = $.trim($(this).text());
+        if (window.language != languageChanged)
+        {
+            bootbox.confirm("Any unsaved changes will be lost. Are you sure you want to change the language?", function(result)
+            {
+                if (result)
+                {
+                    console.log("ste ::" + languageChanged);
+                    loadAllViewsAgainBasedOnLanguage(languageChanged);
+                }
+            });
+        }
+    });
 });
 
+var showVoiceBox = (function()
+{
+    return {
+        configure: function(msg, timeoutlimit)
+        {
+            $('#statusmsg').html(msg);
+            $('#statusLoader').removeClass('hide');
+            $('#statusLoader .voicebox-content').addClass('in');
+            if (timeoutlimit != undefined && timeoutlimit != '')
+            {
+                setTimeout(function()
+                {
+                    $('#statusLoader .voicebox-content').removeClass('in');
+                }, timeoutlimit);
+            }
+        },
+    };
 
+})();
 
+var currentPage = (function()
+{
+    return {
+        get: function()
+        {
+            return document.URL;
+        },
+    };
 
-
+})();
