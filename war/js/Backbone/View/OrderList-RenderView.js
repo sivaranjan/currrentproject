8b7f98ef96cd403f2c39eaf5e931635d726c288e
Sathya Narayanan <sathyanarayanan.anbu@valeo.com>
2016-09-05 16:09:05 +0530

    test

--- war/js/Backbone/View/OrderList-RenderView.js
+++ war/js/Backbone/View/OrderList-RenderView.js
@@ -1,40 +1,69 @@
-BackboneData.Views.OrderListview = Backbone.View.extend({
+BackboneData.Views.OrderListview = Backbone.View.extend(
+{
     el: '#orderlist-section',
-    render: function() {
+    render: function()
+    {
         var ref = this;
         var orderListViewHtml = "";
         orderListViewHtml = orderlist.manager.OrderListDetailView();
         ref.$el.html(orderListViewHtml);
     },
-    initialize: function(name) {
+    initialize: function(name)
+    {
         var _thisView = this;
         _thisView.render();
         this.buildOrderListTable();
     },
-    buildOrderListTable: function() {
-    	$('#orderlist-table').DataTable({
-    		ajax: "http://localhost:8888/fetchController/fetchOrders",
+    buildOrderListTable: function()
+    {
+        $('#orderlist-table').DataTable(
+        {
+            ajax: ApplicationConstants.fetchOrdersList,
             bDestroy: true,
             columns: [
-                        { "data": "site_Workshop_Prototype" },
-                        { "data": "no_prototype_order" },
-                        { "data": "order_Status" },
-                        { "data": "branch_Code" },
-                        { "data": "no_customerOrder" },
-                        { "data": "projectManager" },
-                        { "data": "proto_Type" },
-                        { "data": "type_of_the_Prototype_Order" },
-                        { "data": "date_of_the_Order" },
-                        { "data": "requester" },
-                        { "data": "" }
-            ],
-            responsive: {
-                details: {
+            {
+                "data": "site_Workshop_Prototype"
+            },
+            {
+                "data": "no_prototype_order"
+            },
+            {
+                "data": "order_Status"
+            },
+            {
+                "data": "branch_Code"
+            },
+            {
+                "data": "no_customerOrder"
+            },
+            {
+                "data": "projectManager"
+            },
+            {
+                "data": "proto_Type"
+            },
+            {
+                "data": "type_of_the_Prototype_Order"
+            },
+            {
+                "data": "date_of_the_Order"
+            },
+            {
+                "data": "requester"
+            },
+            {
+                "data": ""
+            }],
+            responsive:
+            {
+                details:
+                {
                     type: 'column',
                     target: -1
                 }
             },
-            columnDefs: [{
+            columnDefs: [
+            {
                 className: 'control',
                 orderable: false,
                 targets: -1
