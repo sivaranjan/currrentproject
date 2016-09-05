50aa7d3a588dee35b2cf5d33645d5b1dd5dae93f
Sathya Narayanan <sathyanarayanan.anbu@valeo.com>
2016-09-05 16:08:49 +0530

    test

--- war/js/Backbone/View/Welcome-RenderView.js
+++ war/js/Backbone/View/Welcome-RenderView.js
@@ -1,22 +1,29 @@
-BackboneData.Views.WelcomeView = Backbone.View.extend({
+BackboneData.Views.WelcomeView = Backbone.View.extend(
+{
     el: '#welcome-section',
-    render: function() {
+    render: function()
+    {
         var ref = this;
         var welcomeHTML = "";
-        if (window.language == "EN") {
-            welcomeHTML = welcome.manager.WelcomeDetailView({ 'paramValue': window.welcome_englishLabelList });
-        } else {
-            welcomeHTML = welcome.manager.WelcomeDetailView({ 'paramValue': window.welcome_frenchLabelList });
+        if (window.language == "EN")
+        {
+            welcomeHTML = welcome.manager.WelcomeDetailView(
+            {
+                'paramValue': window.welcome_englishLabelList
+            });
+        }
+        else
+        {
+            welcomeHTML = welcome.manager.WelcomeDetailView(
+            {
+                'paramValue': window.welcome_frenchLabelList
+            });
         }
         ref.$el.html(welcomeHTML);
     },
-	initialize: function() {
-	    _.bindAll(this, 'cleanup');
-	    var _thisView = this;
-	    _thisView.render();
-	},
-	cleanup: function() {
-	    this.undelegateEvents();
-	    $(this.el).empty();
-	}
+    initialize: function()
+    {
+        var _thisView = this;
+        _thisView.render();
+    }
 });
