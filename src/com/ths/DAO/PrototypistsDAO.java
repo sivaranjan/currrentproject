53854af691f788a7503476aa8d6a18a2af89678a
Sathya <sathyanarayanan.anbu@valeo.com>
2016-09-06 07:46:22 +0530

    test

---
+++ src/com/ths/DAO/PrototypistsDAO.java
@@ -0,0 +1,39 @@
+package com.ths.DAO;
+
+import static java.util.logging.Level.FINER;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.googlecode.objectify.Key;
+import com.ths.JDO.PrototypistsJDO;
+
+public class PrototypistsDAO extends AbstractDao {
+
+    private static final Logger log = Logger.getLogger(PrototypistsDAO.class.getName());
+
+    public PrototypistsJDO findById(long id) {
+        log.log(FINER, "Loading example with ID {0}", id);
+        return ofy.load().key(Key.create(PrototypistsJDO.class, id)).now();
+    }
+
+    public PrototypistsJDO findByName(String prototypist) {
+        return ofy.load().type(PrototypistsJDO.class).filter("prototypist =", prototypist).first().now();
+    }
+
+    public List<PrototypistsJDO> findAllUsers() {
+        log.log(FINER, "Loading all examples.");
+        return ofy.load().type(PrototypistsJDO.class).list();
+    }
+
+    public PrototypistsJDO save(PrototypistsJDO prototypist) {
+        log.log(FINER, "Saving example '{0}'");
+        ofy.save().entities(prototypist).now();
+        return prototypist;
+    }
+    
+    public void deleteAll() {
+        List<Key<PrototypistsJDO>> keys = ofy.load().type(PrototypistsJDO.class).keys().list();
+        ofy.delete().entities(keys).now();
+    }
+}
