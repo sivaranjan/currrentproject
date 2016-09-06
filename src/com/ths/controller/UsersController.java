f55b6b401deac9286afc693546bf1d8573118f09
Sathya <sathyanarayanan.anbu@valeo.com>
2016-09-06 07:50:08 +0530

    test

--- src/com/ths/controller/UsersController.java
+++ src/com/ths/controller/UsersController.java
@@ -4,18 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.ths.DAO.PlacesDAO;
 import com.ths.DAO.UserDAO;
-import com.ths.JDO.PlacesJDO;
 import com.ths.JDO.UserJDO;
 
 
