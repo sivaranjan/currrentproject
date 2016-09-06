43d8e61fb434a28c62e979df02bd8d9fb37e22b1
Sathya Narayanan <sathyanarayanan.anbu@valeo.com>
2016-09-06 20:14:29 +0530

    test

--- src/com/ths/controller/OrderController.java
+++ src/com/ths/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.ths.controller;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.ths.DAO.IdDAO;
 import com.ths.DAO.OrderDAO;
+import com.ths.JDO.IdJDO;
 import com.ths.JDO.OrderJDO;
 @Controller
 @RequestMapping("/order")
@@ -18,12 +23,29 @@ public class OrderController {
 
     @Autowired
     private OrderDAO orderDao;
+    @Autowired
+    private IdDAO idDao;
+    private static final Logger log = Logger.getLogger(OrderController.class.getName());
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public ResponseEntity<Void> createUser(@RequestBody OrderJDO order,UriComponentsBuilder ucBuilder) {
+    public ResponseEntity<Void> createUser(@RequestBody OrderJDO order,UriComponentsBuilder ucBuilder) 
+    {
         System.out.println("Creating User ");
-  
-        orderDao.save(order);
-  
+        List<IdJDO> idlist = null;
+        try
+        {
+        	 orderDao.save(order);
+        	 idlist = idDao.findallEntries();
+        	 for (IdJDO obj :idlist )
+        	 {
+        		 obj.setNext_id(obj.getNext_id()+1);
+        		 idDao.update(obj);
+        	 }
+        }
+        catch(Exception e)
+        {
+        	log.log(java.util.logging.Level.SEVERE, e.getMessage(), e);
+        }
+       
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
