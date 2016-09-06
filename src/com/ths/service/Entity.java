4eea56aa3eae3e72e3658adbf029fdac34d353ce
Sathya <sathyanarayanan.anbu@valeo.com>
2016-09-06 07:50:27 +0530

    test

--- src/com/ths/service/Entity.java
+++ src/com/ths/service/Entity.java
@@ -2,6 +2,8 @@ package com.ths.service;
 
 import com.googlecode.objectify.ObjectifyService;
 import com.ths.JDO.ActorsListJDO;
+import com.ths.JDO.AllocationTurnOverJDO;
+import com.ths.JDO.ClientLaboJDO;
 import com.ths.JDO.ComponentIDJDO;
 import com.ths.JDO.CustomersListJDO;
 import com.ths.JDO.Example;
@@ -10,7 +12,11 @@ import com.ths.JDO.IncotermsJDO;
 import com.ths.JDO.OrderJDO;
 import com.ths.JDO.PlacesJDO;
 import com.ths.JDO.PlateformJDO;
+import com.ths.JDO.ProductTypesJDO;
+import com.ths.JDO.PrototypistsJDO;
+import com.ths.JDO.RandDJDO;
 import com.ths.JDO.SitesListJDO;
+import com.ths.JDO.TechnologyJDO;
 import com.ths.JDO.UserJDO;
 
 public class Entity {
@@ -35,6 +41,13 @@ public class Entity {
         ObjectifyService.register(IdJDO.class);
         ObjectifyService.register(UserJDO.class);
         ObjectifyService.register(ComponentIDJDO.class);
+        ObjectifyService.register(ProductTypesJDO.class);
+        ObjectifyService.register(PrototypistsJDO.class);
+        ObjectifyService.register(RandDJDO.class);
+        ObjectifyService.register(TechnologyJDO.class);
+        ObjectifyService.register(AllocationTurnOverJDO.class);
+        ObjectifyService.register(ClientLaboJDO.class);
+        
     }
 
 }
