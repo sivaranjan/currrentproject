53048a8375972f0e01b4a4b56b410a4d0f323508
Sathya <sathyanarayanan.anbu@valeo.com>
2016-09-06 07:52:11 +0530

    test

--- war/js/Loader/constants.js
+++ war/js/Loader/constants.js
@@ -2,28 +2,46 @@ window.ApplicationConstants || (window.ApplicationConstants = {});
 
 ApplicationConstants = {
 
-"fetchActorsListByType"	: 	"/fetchController/fetchActorsByType/",
-"fetchIncotermsList" 	: 	"/fetchController/fetchIncotermsList",
-"fetchCustomersList"	: 	"/fetchController/fetchCustomersList",
-"fetchPlateformesList"	: 	"/fetchController/fetchPlateformList",
-"fetchPlacesList"		:	"/fetchController/fetchPlacesList",
-"fetchSitesList"		:	"/fetchController/fetchSitesList",
-"fetchIDList"			:	"/fetchController/fetchIDList",
-"fetchComponentIDList"	:	"/fetchController/fetchComponentIDList",
-"fetchActorsListBySite"	:	"/fetchController/fetchActorsListBySite/",
-"fetchCustomerDetailsbyName":	"/fetchController/fetchCustomerDetailsbyName/",
-"fetchUserInfoDetails"	:	"/fetchController/fetchUserDetails/",
-"fetchOrderDependencies": 	"/fetchController/fetchOrderDependencies",
-"fetchOrdersList"		: 	"/fetchController/fetchOrders",
-"saveOrderUrl"			:	"/order/create",
-"createorupdateuser"	:	"/users/createorupdate",
-"saveActorsListURL"		:	"/actorsList/create",
-"saveCustomersListURL"	:	"/customerslist/create",
-"saveIncotermsListURL"	:	"/incoterm/create",
-"savePlacesListURL"		:	"/place/create",
-"savePlaterormsListURL"	:	"/plateform/create",
-"savesSitesListURL"		:	"/siteslist/create",
-"generateNewId"			:	"/idgenerator/createNewID"
+"fetchActorsListByType"			: 	"/fetchController/fetchActorsByType/",
+"fetchIncotermsList" 			: 	"/fetchController/fetchIncotermsList",
+"fetchCustomersList"			: 	"/fetchController/fetchCustomersList",
+"fetchPlateformesList"			: 	"/fetchController/fetchPlateformList",
+"fetchPlacesList"				:	"/fetchController/fetchPlacesList",
+"fetchSitesList"				:	"/fetchController/fetchSitesList",
+"fetchIDList"					:	"/fetchController/fetchIDList",
+"fetchComponentIDList"			:	"/fetchController/fetchComponentIDList",
+"fetchActorsListBySite"			:	"/fetchController/fetchActorsListBySite/",
+"fetchCustomerDetailsbyName"	:	"/fetchController/fetchCustomerDetailsbyName/",
+"fetchUserInfoDetails"			:	"/fetchController/fetchUserDetails/",
+"fetchOrderDependencies"		: 	"/fetchController/fetchOrderDependencies",
+"fetchOrdersList"				: 	"/fetchController/fetchOrders",
+"fetchProductTypeList"			: 	"/fetchController/fetchProductTypeList",
+"fetchPrototypitsList"			: 	"/fetchController/fetchPrototypitsList",
+"fetchRandDList"				:	"/fetchController/fetchRandDList",
+"fetchTechnologyList"			: 	"/fetchController/fetchTechnologyList",
+"fetchClientLaboList"			: 	"/fetchController/fetchClientLaboList",
+"fetchAllocationofTurnOverList"	: 	"/fetchController/fetchAllocationofTurnOverList",
+"saveOrderUrl"					:	"/order/create",
+"createorupdateuser"			:	"/users/createorupdate",
+"saveActorsListURL"				:	"/actorsList/create",
+"saveCustomersListURL"			:	"/customerslist/create",
+"saveIncotermsListURL"			:	"/incoterm/create",
+"savePlacesListURL"				:	"/place/create",
+"savePlaterormsListURL"			:	"/plateform/create",
+"savesSitesListURL"				:	"/siteslist/create",
+"saveProductTypeListURL"		:	"/producttype/create",
+"savePrototypistListURL"		:	"/prototypist/create",
+"saveTechnologyListURL"			:	"/technology/create",
+"saveRandDListURL"				:	"/rAndDController/create",
+"saveAllocationListURL"			:	"/allocationController/create",
+"saveClientLaboListURL"			:	"/clientLabo/create",
+"generateNewId"					:	"/idgenerator/createNewID"
+	
+	
+	
+	
+	
+	
 	
 }
 
