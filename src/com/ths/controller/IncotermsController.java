8a4e520b1df701b219e53b2f78c93e54bd607ef8
Sathya <sathyanarayanan.anbu@valeo.com>
2016-09-06 07:49:35 +0530

    test

--- src/com/ths/controller/IncotermsController.java
+++ src/com/ths/controller/IncotermsController.java
@@ -5,11 +5,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ths.DAO.IncotermsDAO;
@@ -23,29 +21,12 @@ public class IncotermsController {
     @Autowired
     private IncotermsDAO incotermsDao;
 
-//
-//    @RequestMapping("/create")
-//    @ResponseBody
-//    public Example link() {
-//        Example example = new Example();
-////        example.setName("Example");
-//        return exampleDao.save(example);
-//    }
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity<Void> createUser(@RequestBody IncotermsJDO incoterm, UriComponentsBuilder ucBuilder) {
         System.out.println("Incoterms details");
   
         incotermsDao.save(incoterm);
-//        if (userService.isUserExist(user)) 
-//        {
-//            System.out.println("A User with name " + user.getName() + " already exist");
-//            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-//        }
-//  
-//        userService.saveUser(user);
-  
         HttpHeaders headers = new HttpHeaders();
-//        headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
 
