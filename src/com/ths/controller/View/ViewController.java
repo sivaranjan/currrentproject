61dea4ccb31f4e595cc6a78d0fd563efa913446e
Sathya Narayanan <sathyanarayanan.anbu@valeo.com>
2016-09-12 20:26:51 +0530

    test

---
+++ src/com/ths/controller/View/ViewController.java
@@ -0,0 +1,17 @@
+package com.ths.controller.View;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping("/")
+public class ViewController {
+ 
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "layout";
+    }
+}
